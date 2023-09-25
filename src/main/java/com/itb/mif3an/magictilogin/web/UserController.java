3e970446feb8c97fa3b8cd6d40b8df28d4ab1f9c
Gessivan Junior <priceddread13@gmail.com>
2023-09-25 20:32:47 -0300

    em processo de construcao do perfil

--- src/main/java/com/itb/mif3an/magictilogin/web/UserController.java
+++ src/main/java/com/itb/mif3an/magictilogin/web/UserController.java
@@ -3,10 +3,13 @@ package com.itb.mif3an.magictilogin.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.itb.mif3an.magictilogin.model.User;
 import com.itb.mif3an.magictilogin.service.UserService;
+import com.itb.mif3an.magictilogin.web.dto.UserDto;
 
 
 	
@@ -30,6 +33,17 @@ import com.itb.mif3an.magictilogin.service.UserService;
 		model.addAttribute("username", username);
 		
 		return home;
+	}
+	
+	@GetMapping("/users/perfil/{username}")
+	public String showPerfilForm(@PathVariable("username") String username, ModelMap model) {
+
+		UserDto userDto = new UserDto();
+		userDto.setEmail(username);
+		User user = userService.findByEmail(userDto.getEmail());
+		model.addAttribute("user", user);
+
+		return "update-registration";
 	}
 
 }
